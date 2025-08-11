4df9dc5348392c0dfd7e8ceb236024aeca0dcafe
Kenji Longid <kmlongid@cpp.edu>
2025-08-11 13:45:19 -0500

    Wrap .mat file in class

--- read_mat.cpp
+++ read_mat.cpp
@@ -4,75 +4,112 @@
 #include <vector>
 #include <iostream>
 
-void read_suitesparse(const char *mat_file_name, const char *arr, const char *field)
+class SuiteSparseMatrix
 {
-    MATFile *mfPtr;
-    mxArray *aPtr;
-
-    mfPtr = matOpen(mat_file_name, "r");
-    if (mfPtr == NULL)
+public:
+    size_t *jc()
     {
-        std::cerr << "Error opening file" << mat_file_name << std::endl;
-        exit(1);
+        return mxGetJc(A); // Column pointers (size cols+1)
     }
 
-    aPtr = matGetVariable(mfPtr, arr);
-    if (aPtr == NULL)
+    size_t *ir()
     {
-        std::cerr << "mxArray not found:" << arr << std::endl;
-        exit(1);
+        return mxGetIr(A); // Row indices (size nnz)
     }
 
-    if (mxGetClassID(aPtr) != mxSTRUCT_CLASS)
+    double *data()
     {
-        std::cerr << arr << " is not a structure" << std::endl;
+        return mxGetPr(A);
+    }
+
+    size_t cols()
+    {
+        return mxGetN(A);
+    }
+
+    size_t rows()
+    {
+        return mxGetM(A);
+    }
+
+    size_t nnz()
+    {
+        return jc()[cols()];
+    }
+
+    size_t data_width()
+    {
+        return mxGetElementSize(A);
+    }
+
+    size_t size()
+    {
+        return mxGetNumberOfElements(A);
+    }
+
+    SuiteSparseMatrix(const char *mat_file_name, const char *arr, const char *field)
+    {
+
+        mfPtr = matOpen(mat_file_name, "r");
+        if (mfPtr == NULL)
+        {
+            std::cerr << "Error opening file" << mat_file_name << std::endl;
+            exit(1);
+        }
+
+        aPtr = matGetVariable(mfPtr, arr);
+        if (aPtr == NULL)
+        {
+            std::cerr << "mxArray not found:" << arr << std::endl;
+            exit(1);
+        }
+
+        if (mxGetClassID(aPtr) != mxSTRUCT_CLASS)
+        {
+            std::cerr << arr << " is not a structure" << std::endl;
+            mxDestroyArray(aPtr);
+            exit(1);
+        }
+
+        if (mxGetFieldNumber(aPtr, field) == -1)
+        {
+            std::cerr << "Field not found:" << field << std::endl;
+            mxDestroyArray(aPtr);
+            exit(1);
+        }
+
+        A = mxGetField(aPtr, 0, field);
+
+        if (!mxIsSparse(A))
+        {
+            std::cerr << "Matrix is not sparse!" << std::endl;
+            return;
+        }
+
+        mwSize rows = mxGetM(A);
+        mwSize cols = mxGetN(A);
+        if (rows != cols)
+        {
+            throw std::runtime_error("error reading SPD mat: m != n");
+        }
+    }
+
+    ~SuiteSparseMatrix()
+    {
         mxDestroyArray(aPtr);
-        exit(1);
+        mxDestroyArray(A);
+        if (matClose(mfPtr) != 0)
+        {
+            std::cerr << "Error closing mat file" << std::endl;
+            exit(1);
+        }
     }
 
-    if (mxGetFieldNumber(aPtr, field) == -1)
-    {
-        std::cerr << "Field not found:" << field << std::endl;
-        mxDestroyArray(aPtr);
-        exit(1);
-    }
-
+private:
+    MATFile *mfPtr = nullptr;
+    mxArray *aPtr = nullptr;
     mxArray *A = nullptr;
-    A = mxGetField(aPtr, 0, field);
-
-    if (!mxIsSparse(A))
-    {
-        std::cerr << "Matrix is not sparse!" << std::endl;
-        return;
-    }
-
-    mwSize rows = mxGetM(A);
-    mwSize cols = mxGetN(A);
-    if (rows != cols)
-    {
-        throw std::runtime_error("error reading SPD mat: m != n");
-    }
-
-    mwIndex *jc = mxGetJc(A); // Column pointers (size cols+1)
-    mwIndex *ir = mxGetIr(A); // Row indices (size nnz)
-    double *A_data = mxGetPr(A);  // Non-zero values (size nnz)
-
-    mwSize nnz = jc[cols];
-
-    mwSize data_width = mxGetElementSize(A);
-    mwSize size = mxGetNumberOfElements(A);
-
-    std::cout << "Size: " << size << std::endl;
-    std::cout << "# Non-Zeros: " << nnz << std::endl;
-
-    mxDestroyArray(aPtr);
-    mxDestroyArray(A);
-    if (matClose(mfPtr) != 0)
-    {
-        std::cerr << "Error closing file" << mat_file_name << std::endl;
-        exit(1);
-    }
-}
+};
 
 int main(int argc, char const *argv[])
 {
@@ -81,5 +118,14 @@ int main(int argc, char const *argv[])
         std::cerr << "Invalid arguments" << std::endl;
         return 1;
     }
-    read_suitesparse(argv[1], "Problem", "A");
+    SuiteSparseMatrix mat(argv[1], "Problem", "A");
+
+    std::cout << "jc: " << mat.jc() << std::endl;
+    std::cout << "ir: " << mat.ir() << std::endl;
+    std::cout << "data: " << mat.data() << std::endl;
+    std::cout << "cols: " << mat.cols() << std::endl;
+    std::cout << "rows: " << mat.rows() << std::endl;
+    std::cout << "data_width: " << mat.data_width() << std::endl;
+    std::cout << "size: " << mat.size() << std::endl;
+    std::cout << "nnz: " << mat.nnz() << std::endl;
 }
