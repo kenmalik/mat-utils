75fd168fb8802c055378bdeb3b7df64fa59f5ca2
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-10-15 09:54:53 -0700

    Use unique_ptr for mat file pointer

--- lib/mat_reader.cpp
+++ lib/mat_reader.cpp
@@ -9,10 +9,13 @@
 auto mxArray_deleter = [](mxArray *ptr) { mxDestroyArray(ptr); };
 using mxArrayPtr = std::unique_ptr<mxArray, decltype(mxArray_deleter)>;
 
+auto matFile_deleter = [](MATFile *ptr) { matClose(ptr); };
+using MATFilePtr = std::unique_ptr<MATFile, decltype(matFile_deleter)>;
+
 namespace mat_utils {
 
 struct MatReader::MatReaderImpl {
-  MATFile *mat_file_ptr = nullptr;
+  MATFilePtr mat_file_ptr{nullptr, matFile_deleter};
   mxArrayPtr A_ptr{nullptr, mxArray_deleter};
 };
 
@@ -24,13 +27,13 @@ MatReader::MatReader(const std::string &mat_file_name,
 
   std::stack<mxArrayPtr> open_structs{};
 
-  impl->mat_file_ptr = matOpen(mat_file_name.c_str(), "r");
+  impl->mat_file_ptr.reset(matOpen(mat_file_name.c_str(), "r"));
   if (impl->mat_file_ptr == NULL) {
     throw std::runtime_error("Error opening file "s + mat_file_name);
   }
 
   if (structs.empty()) {
-    mxArray *A = matGetVariable(impl->mat_file_ptr, field.c_str());
+    mxArray *A = matGetVariable(impl->mat_file_ptr.get(), field.c_str());
     impl->A_ptr.reset(mxDuplicateArray(A));
 
     if (impl->A_ptr.get() == NULL) {
@@ -50,8 +53,8 @@ MatReader::MatReader(const std::string &mat_file_name,
   if (structs_iter != structs.end()) {
     auto arr = *structs_iter;
 
-    mxArrayPtr mat_variable{matGetVariable(impl->mat_file_ptr, arr.c_str()),
-                            mxArray_deleter};
+    mxArrayPtr mat_variable{
+        matGetVariable(impl->mat_file_ptr.get(), arr.c_str()), mxArray_deleter};
     if (mat_variable.get() == NULL) {
       throw std::invalid_argument("mxArray not found "s + arr);
     }
@@ -102,14 +105,9 @@ MatReader::MatReader(const std::string &mat_file_name,
   }
 }
 
-MatReader::~MatReader() { close(); }
+MatReader::~MatReader() = default;
 
-void MatReader::close() {
-  if (matClose(impl->mat_file_ptr) != 0) {
-    std::cerr << "Error closing mat file" << std::endl;
-    exit(1);
-  }
-}
+void MatReader::close() { impl.reset(); }
 
 size_t MatReader::cols() { return mxGetN(impl->A_ptr.get()); }
 
