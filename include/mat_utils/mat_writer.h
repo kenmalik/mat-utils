cfa67d83dfee03e5858cca2c97195d32ef8e46c1
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-11-28 10:27:35 -0800

    Revert "Double mat writer"

--- include/mat_utils/mat_writer.h
+++ include/mat_utils/mat_writer.h
@@ -21,10 +21,6 @@ class MatWriter {
     void write_dense(const std::string &name, const std::vector<float> &matrix,
                      size_t rows, size_t cols);
 
-    void write_dense(const std::string &name, const std::vector<double> &matrix,
-                     size_t rows, size_t cols);
-
-
   private:
     MatWriterImpl *impl;
 };
