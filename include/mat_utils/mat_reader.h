59c86fa9c041486af93f6be727d43faeb4a5df05
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-07-22 16:52:31 -0700

    Move MatReader implementation to header

--- include/mat_utils/mat_reader.h
+++ include/mat_utils/mat_reader.h
@@ -1,50 +1,150 @@
 #pragma once
 
 #include <memory>
+#include <stack>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
+#include <mat.h>
+#include <matrix.h>
+
 namespace mat_utils {
+
+namespace detail {
+
+inline void destroy_mxArray(mxArray *ptr) { mxDestroyArray(ptr); }
+using mxArrayPtr = std::unique_ptr<mxArray, decltype(&destroy_mxArray)>;
+
+inline void close_mat_file(MATFile *ptr) { matClose(ptr); }
+using MATFilePtr = std::unique_ptr<MATFile, decltype(&close_mat_file)>;
+
+} // namespace detail
 
 class MatReader {
   public:
     MatReader(const std::string &mat_file_name,
-              const std::vector<std::string> &arr, const std::string &field);
-    virtual ~MatReader();
+              const std::vector<std::string> &structs, const std::string &field)
+        : impl(std::make_unique<MatReaderImpl>()) {
+        using namespace std::string_literals;
 
-    MatReader(MatReader &&rhs) noexcept;
-    MatReader &operator=(MatReader &&rhs) noexcept;
+        std::stack<detail::mxArrayPtr> open_structs{};
 
-    size_t cols() const;
-    size_t rows() const;
-    size_t data_width() const;
-    size_t size() const;
-    void close();
-    double *data() const;
+        impl->mat_file_ptr.reset(matOpen(mat_file_name.c_str(), "r"));
+        if (impl->mat_file_ptr == NULL) {
+            throw std::runtime_error("Error opening file "s + mat_file_name);
+        }
+
+        if (structs.empty()) {
+            mxArray *A =
+                matGetVariable(impl->mat_file_ptr.get(), field.c_str());
+            impl->A_ptr.reset(mxDuplicateArray(A));
+
+            if (impl->A_ptr.get() == NULL) {
+                throw std::invalid_argument("mxArray not found "s + field);
+            }
+
+            return;
+        }
+
+        auto structs_iter = structs.begin();
+        if (structs_iter != structs.end()) {
+            auto arr = *structs_iter;
+
+            detail::mxArrayPtr mat_variable{
+                matGetVariable(impl->mat_file_ptr.get(), arr.c_str()),
+                detail::destroy_mxArray};
+            if (mat_variable.get() == NULL) {
+                throw std::invalid_argument("mxArray not found "s + arr);
+            }
+
+            if (mxGetClassID(mat_variable.get()) != mxSTRUCT_CLASS) {
+                throw std::invalid_argument(arr + " is not a structure"s);
+            }
+
+            open_structs.push(std::move(mat_variable));
+            ++structs_iter;
+        }
+
+        for (; structs_iter != structs.end(); ++structs_iter) {
+            auto current = open_structs.top().get();
+            auto arr = *structs_iter;
+
+            if (mxGetFieldNumber(current, arr.c_str()) == -1) {
+                throw std::invalid_argument("field not found: "s + arr);
+            }
+
+            constexpr int INDEX = 0;
+            detail::mxArrayPtr next_struct{
+                mxGetField(current, INDEX, arr.c_str()),
+                detail::destroy_mxArray};
+            if (next_struct.get() == NULL) {
+                throw std::invalid_argument("mxArray not found "s + arr);
+            }
+
+            if (mxGetClassID(next_struct.get()) != mxSTRUCT_CLASS) {
+                throw std::invalid_argument(arr + " is not a structure"s);
+            }
+
+            open_structs.push(std::move(next_struct));
+        }
+
+        auto last_struct = open_structs.top().get();
+        if (mxGetFieldNumber(last_struct, field.c_str()) == -1) {
+            throw std::invalid_argument("field not found: "s + field);
+        }
+
+        constexpr int INDEX = 0;
+        mxArray *A = mxGetField(open_structs.top().get(), INDEX, field.c_str());
+        impl->A_ptr.reset(mxDuplicateArray(A));
+    }
+
+    virtual ~MatReader() = default;
+
+    MatReader(MatReader &&rhs) noexcept = default;
+    MatReader &operator=(MatReader &&rhs) noexcept = default;
+
+    size_t cols() const { return mxGetN(impl->A_ptr.get()); }
+    size_t rows() const { return mxGetM(impl->A_ptr.get()); }
+    size_t data_width() const { return mxGetElementSize(impl->A_ptr.get()); }
+    size_t size() const { return mxGetNumberOfElements(impl->A_ptr.get()); }
+    void close() { impl.reset(); }
+    double *data() const { return mxGetDoubles(impl->A_ptr.get()); }
 
   protected:
-    // We use the PIMPL idiom to avoid having to include MATLAB headers into
-    // application code
-    struct MatReaderImpl;
+    struct MatReaderImpl {
+        detail::MATFilePtr mat_file_ptr{nullptr, detail::close_mat_file};
+        detail::mxArrayPtr A_ptr{nullptr, detail::destroy_mxArray};
+    };
     std::unique_ptr<MatReaderImpl> impl;
 };
 
 class DnMatReader : public MatReader {
   public:
     DnMatReader(const std::string &mat_file_name,
-                const std::vector<std::string> &arr, const std::string &field);
+                const std::vector<std::string> &arr, const std::string &field)
+        : MatReader(mat_file_name, arr, field) {
+        if (mxIsSparse(impl->A_ptr.get())) {
+            throw std::invalid_argument("matrix is sparse");
+        }
+    }
 };
 
 class SpMatReader : public MatReader {
   public:
-    size_t *jc() const;
-    size_t jc_size() const;
-    size_t *ir() const;
-    size_t ir_size() const;
-    size_t nnz() const;
+    size_t *jc() const { return mxGetJc(impl->A_ptr.get()); }
+    size_t jc_size() const { return cols() + 1; }
+    size_t *ir() const { return mxGetIr(impl->A_ptr.get()); }
+    size_t ir_size() const { return nnz(); }
+    size_t nnz() const { return jc()[cols()]; }
 
     SpMatReader(const std::string &mat_file_name,
-                const std::vector<std::string> &arr, const std::string &field);
+                const std::vector<std::string> &arr, const std::string &field)
+        : MatReader(mat_file_name, arr, field) {
+        if (!mxIsSparse(impl->A_ptr.get())) {
+            throw std::invalid_argument("matrix is not sparse");
+        }
+    }
 };
 
 } // namespace mat_utils
